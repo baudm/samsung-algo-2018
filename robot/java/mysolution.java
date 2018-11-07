2b51823c8e395a7729a513f63b1a12e64c7d474b
Darwin Bautista <daruuin@gmail.com>
2018-11-07 15:27:00 +0800

    Change line endings

--- robot/java/mysolution.java
+++ robot/java/mysolution.java
@@ -1,54 +1,45 @@
-import java.util.Map;
-import java.util.HashMap;
-
 @SuppressWarnings("unchecked")
 public class mysolution {
 
-    private static final Map<Character, String> m = new HashMap<>();
-    static {
-        m.put('a', "2");
-        m.put('b', "22");
-        m.put('c', "222");
-        
-        m.put('d', "3");
-        m.put('e', "33");
-        m.put('f', "333");
-        
-        m.put('g', "4");
-        m.put('h', "44");
-        m.put('i', "444");
-        
-        m.put('j', "5");
-        m.put('k', "55");
-        m.put('l', "555");
-        
-        m.put('m', "6");
-        m.put('n', "66");
-        m.put('o', "666");
-        
-        m.put('p', "7");
-        m.put('q', "77");
-        m.put('r', "777");
-        m.put('s', "7777");
-        
-        m.put('t', "8");
-        m.put('u', "88");
-        m.put('v', "888");
-        
-        m.put('w', "9");
-        m.put('x', "99");
-        m.put('y', "999");
-        m.put('z', "9999");
+    private static String getPresses(char c) {
+        switch (c) {
+            case 'a': return "2";
+            case 'b': return "22";
+            case 'c': return "222";
+            case 'd': return "3";
+            case 'e': return "33";
+            case 'f': return "333";
+            case 'g': return "4";
+            case 'h': return "44";
+            case 'i': return "444";
+            case 'j': return "5";
+            case 'k': return "55";
+            case 'l': return "555";
+            case 'm': return "6";
+            case 'n': return "66";
+            case 'o': return "666";
+            case 'p': return "7";
+            case 'q': return "77";
+            case 'r': return "777";
+            case 's': return "7777";
+            case 't': return "8";
+            case 'u': return "88";
+            case 'v': return "888";
+            case 'w': return "9";
+            case 'x': return "99";
+            case 'y': return "999";
+            case 'z': return "9999";
+        }
+        return "";
     }
-
-	static int solve(int N, char[] message, char[] presses) {
+    static int solve(int N, char[] message, char[] presses) {
         int num_presses = 0;
         for (int i = 0; i < N; i++) {
-            final String p = m.get(message[i]);
+            final String p = getPresses(message[i]);
             final int len = p.length();
             p.getChars(0, len, presses, num_presses);
             num_presses += len;
         }
-	    return num_presses;
-	}
+        return num_presses;
+    }
 }
