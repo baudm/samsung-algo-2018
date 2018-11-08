61d5a1f57a9f23ca640f4adddf93f5bac8ab2d32
Darwin Bautista <daruuin@gmail.com>
2018-11-08 14:29:37 +0800

    19/20 feast

--- samsung-feast/c++/mysolution.cpp
+++ samsung-feast/c++/mysolution.cpp
@@ -1,80 +1,40 @@
-#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <cstdint>
 
 int solve(int N, char A[2][10001]) {
-	// counters for the first line
-	unsigned int I1 = 0, E1 = 0, L1 = 0;
-	// counters for the second line
-	unsigned int I2 = 0, E2 = 0, L2 = 0;
-
-	for (int i = 0; i < N; i++) {
-		switch (A[0][i]) {
-			case 'I':
-				I1++;
-				break;
-			case 'E':
-				E1++;
-				break;
-			case 'L':
-				L1++;
-		}
-		switch (A[1][i]) {
-			case 'I':
-				I2++;
-				break;
-			case 'E':
-				E2++;
-				break;
-			case 'L':
-				L2++;
-		}
-	}
-
-	unsigned int score = 0;
-
-	unsigned int min = I1 < I2 ? I1 : I2;
-	I1 -= min;
-	I2 -= min;
-	score += min;
-
-	min = E1 < E2 ? E1 : E2;
-	E1 -= min;
-	E2 -= min;
-	score += min;
-
-	// Only one of E1 or I1 will be nonzero at this point
-	if (L2) {
-		if (E1) {
-			min = E1 < L2 ? E1 : L2;
-			E1 -= min;
-		} else {
-			min = I1 < L2 ? I1 : L2;
-			I1 -= min;
-		}
-		//L2 -= min;
-	}
-
-	// Only one of E2 or I2 will be nonzero at this point
-	/*
-	if (L1) {
-		if (E2) {
-			min = E2 < L1 ? E2 : L1;
-			E2 -= min;
-		} else {
-			min = I2 < L1 ? I2 : L1;
-			I2 -= min;
-		}
-		L1 -= min;
-	}
-	*/
-
-	// At this point, both L1 and L2 should be zero.
-	// deduct E-I pairings
-	score -= I1;
-	score -= E1;
-
-
-	//std::cout << I1 << ", " << E1 << ", " << L1 << std::endl;
-	//std::cout << I2 << ", " << E2 << ", " << L2 << std::endl;
-
-	return score;
+    std::vector<int_fast8_t> line1;
+    std::vector<int_fast8_t> line2;
+    
+    // By choosing elements from the set {-1, 0, 1}, and setting E = -1 (or 1), I = 1 (or -1), and L = 0,
+    // we can represent the lines as N-dim vectors, V1 and V2.
+    // The HP can be easily computed by calculating the dot product, i.e. HP = V1 * V2
+    //
+    // We can then model the problem as finding the maximum dot product between V1 (or V2)
+    // and all the cyclic permutations of the other vector.
+    
+    // Lookup table to translate from char values to values suited for vector representation:
+    // The numeric values of E, I, and L are very close to each other, so we can afford to
+    // create a small lookup table and just add dummy values in between.
+    //                              E           I        L
+    static int_fast8_t lookup[] = {-1, 0, 0, 0, 1, 0, 0, 0};
+    
+    for (int i = 0; i < N; i++) {
+        line1.push_back(lookup[A[0][i] - 'E']);
+        line2.push_back(lookup[A[1][i] - 'E']);
+    }
+    
+    int max_score = 0;
+    
+    for (int i = 0; i < N; i++) {
+        int score = std::inner_product(line1.begin(), line1.end(), line2.begin(), 0);
+        if (score > max_score) {
+            max_score = score;
+        }
+        // Get next permutation of V1
+        std::rotate(line1.begin(), line1.begin() + 1, line1.end());
+    }
+    
+    return max_score;
 }
